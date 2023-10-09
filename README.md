6df9c6eb6f1107af7fd52a878791919f2e32f363
Talescruzs <tcs2017@hotmail.com>
2023-10-09 16:48:26 -0300

    criacao de pesquisa tipo xpath in json

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+
+# Detalhes:
+Schema de json criado pelo site: https://www.liquid-technologies.com/online-json-to-schema-converter e editado
 # Passos para rodar no linux:
 1- ter o python instalado
 2- utilizar o comando no terminal: pip install xmltodict 
--- criaJson.py
+++ criaJson.py
@@ -33,7 +33,7 @@ class Files:
 
 class Validator:
     def __init__(self, files:Files, jsonSchemaPath:str):
-        self.validJson = list()
+        self.validJson = dict()
         self.invalidJson = list()
         self.files=files
 
@@ -43,26 +43,84 @@ class Validator:
         self.__validate()
 
     def __validate(self):
-        print(self.files.xmlFiles)
-        print(self.files.jsonFiles)
-        print(self.files.xmlFolder)
-        print(self.files.jsonFolder)
         for currentFile in self.files.jsonFiles:
             with open(self.files.jsonFolder+currentFile, 'r') as file:
                 thisJson = json.load(file)
             
             validator = Draft7Validator(self.schema)
-            print(currentFile, ":\n")
-            print(list(validator.iter_errors(thisJson)))
-            print("\n\n\n")
             
             if(len(list(validator.iter_errors(thisJson)))==0):
-                self.validJson.append(currentFile)
+                self.validJson[currentFile]=thisJson
             else:
                 self.invalidJson.append(currentFile)
-        print(self.invalidJson)
-        print(self.validJson)
+
+class Search:
+    def __init__(self, files:Validator):
+        self.validFiles = files.validJson
+        self.invalidFiles = files.invalidJson
+
+    def __nameFilesProcessing(self, nameFiles):
+        files = list()
+        if(nameFiles=="."):
+            files = list(self.validFiles.values())
+        else:
+            names = nameFiles.split(",")
+            for name in names:
+                files.append(self.validFiles[name])
+        return files
+    
+    def __searchProcessing(self, search):
+        research = list()
+        if("/" in search):
+            research = search.split("/")
+        else:
+            research.append(search)
+        return research
+    
+    def __treeSearch(self, search, file):
+        parsedSearch = file
+        flag = 0
+        results = list()
+        for layer in range(len(search)):
+            try:
+                parsedSearch = parsedSearch[search[layer]]
+            except:
+                flag=1
+                if(type(parsedSearch)==list):
+                    newSearch = list()
+                    for a in range(len(search)):
+                        if(a>=layer):
+                            newSearch.append(search[layer])
+                    for division in parsedSearch:
+                        results.append(self.__treeSearch(newSearch, division))
+                else:
+                    pass
+        if(flag==0):
+            results = parsedSearch
+        return results
+
+    def search(self, search:str, nameFiles="."):
+        results = list()
+        filesSearched = self.__nameFilesProcessing(nameFiles)
+        processedSearch = self.__searchProcessing(search)    
+
+        for file in filesSearched:
+            results.append(self.__treeSearch(processedSearch, file))
+            
+        return results
 
 if __name__ == "__main__":
     teste1 = Files("notasFiscais/", "Json/")
     teste2 = Validator(teste1, "schema.json")
+    #vProd = valor de produto
+    # print(teste2.validJson["nota1.json"]["ns0:nfeProc"]["@xmlns:ns0"])
+    teste3 = Search(teste2)
+    a=0
+    v=0
+    for i in range(len(teste3.search("ns0:nfeProc/ns0:NFe/ns0:infNFe/ns0:det/@nItem"))):
+        valor = teste3.search("ns0:nfeProc/ns0:NFe/ns0:infNFe/ns0:total/ns0:ICMSTot/ns0:vProd")
+        a+=int(teste3.search("ns0:nfeProc/ns0:NFe/ns0:infNFe/ns0:det/@nItem")[i][-1])
+        v+=float(valor[i])
+    print(a)
+    print(v)
+    # teste3.search("ns0:nfeProc/ns0:NFe/ns0:infNFe/ns0:det", "nota5.json")
